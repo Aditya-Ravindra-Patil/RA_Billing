9efa1682f7ebcd7a9dd2bcb863018569b6dfc152
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2026-01-03 23:32:50 +0530

    Add files via upload

---
+++ src/main/java/com/example/RA_Billing/RaBillingApplication.java
@@ -0,0 +1,13 @@
+package com.example.RA_Billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RA_Billing/RaBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RA_Billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
